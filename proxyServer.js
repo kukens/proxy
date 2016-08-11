2b8bc7e84d2cd6ef255b4d6e4acec7a1a66010c7
Lukasz <lukzieb@wp.pl>
2016-08-11 13:24:53 +0200

    fdsfds

--- proxyServer.js
+++ proxyServer.js
@@ -13,46 +13,9 @@ module.exports.init = function () {
 
     proxy.onRequest(function (ctx, callback) {
 
-        var fullUrl = ctx.proxyToServerRequestOptions.agent.protocol + '//' + ctx.clientToProxyRequest.headers.host + ctx.clientToProxyRequest.url;
-        ctx.proxyToClientResponse.setHeader('wptProxyHit', 'true');
-
-        console.log(ctx.clientToProxyRequest.headers.wptproxypolicy);
-        console.log(fullUrl);
-
         ctx.proxyToClientResponse.end("dupa sraka");
 
-        Policy.findOne({ _id: ctx.clientToProxyRequest.headers.wptproxypolicy, 'properties.url': fullUrl }, {"properties.$": 1, _id: 0}).exec(function (err, match) {
-
-            if (err) return console.error(err);
-
-            if (match && match.properties) {
-
-                var property = match.properties[0];
-
-                if (fullUrl == property.url) {
-
-                    property.headers.forEach(function (item) {
-                        ctx.proxyToClientResponse.setHeader(item.name, item.value)
-                    })
-
-                    if (property.body != '') {
-                        ctx.onResponseData(function (ctx, chunk, callback) {
-                            return callback(null, null);
-                        });
-                        zlib.gzip(property.body, function (_, result) {
-                            ctx.clientToProxyRequest.headers.proxyContentLength = result.length;
-
-                            ctx.onResponseEnd(function (ctx, callback) {
-                                ctx.proxyToClientResponse.end(result);
-                            });
-                        });
-                    }
-                }
-            }
-
             //return callback();
-
-        });
     });
     console.log('Starting proxy server...')
     proxy.listen({ port: 8081, keepAlive: true });
