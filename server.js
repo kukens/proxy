dfec560e65c58618cefff1a469b7059c1e1292db
Lukasz <lukzieb@wp.pl>
2016-08-12 17:59:30 +0200

    map

---
+++ server.js
@@ -0,0 +1,30 @@
+﻿var http = require("http");
+
+
+http.createServer(function (request, response) {
+
+    console.log(request.url);
+    console.log(request.headers.host);
+
+    if (request.headers.host == 'psiepodworko.pl')
+    {
+        response.end('psiepodworko <script src="http://www.onet.pl/1.js" type="application/javascript" /> dupcia');
+    }
+
+}).listen(8084);
+
+// curl -k https://localhost:8000/
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+};
+
+https.createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+}).listen(8085);
+
+console.log('listening');
